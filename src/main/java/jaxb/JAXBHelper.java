755b8b84f094487fed064a35e13b1b0ebfe389db
Bartha Zoltán <barthazoli00@gmail.com>
2021-04-25 20:04:10 +0200

    JAXB helper methods added

---
+++ src/main/java/jaxb/JAXBHelper.java
@@ -0,0 +1,35 @@
+package jaxb;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
+import jakarta.xml.bind.Unmarshaller;
+
+public class JAXBHelper {
+
+    public static void toXML(Object o, OutputStream os) throws JAXBException {
+        try {
+            JAXBContext context = JAXBContext.newInstance(o.getClass());
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+            marshaller.marshal(o, os);
+        } catch(JAXBException e) {
+            throw e;
+        }
+    }
+
+    public static <T> T fromXML(Class<T> clazz, InputStream is) throws JAXBException {
+        try {
+            JAXBContext context = JAXBContext.newInstance(clazz);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            return (T) unmarshaller.unmarshal(is);
+        } catch(JAXBException e) {
+            throw e;
+        }
+    }
+
+}
