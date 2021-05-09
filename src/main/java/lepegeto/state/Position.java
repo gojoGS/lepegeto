1999aab77205b22ca1047b8ddc0359db02d501fd
Bartha Zoltán <barthazoli00@gmail.com>
2021-05-09 20:03:45 +0200

    Added docstrings to Position.java

--- src/main/java/lepegeto/state/Position.java
+++ src/main/java/lepegeto/state/Position.java
@@ -2,23 +2,44 @@ package lepegeto.state;
 
 import java.util.Objects;
 
+/**
+ * Represents a 2D position.
+ */
 public class Position implements Cloneable {
     private int row;
     private int col;
 
+    /**
+     * Creates a {@code Position} object.
+     *
+     * @param row the row coordinate of the position
+     * @param col the column coordinate of the position
+     */
     public Position(int row, int col) {
         this.row = row;
         this.col = col;
     }
 
+    /**
+     * {@return the row coordinate of the position}
+     */
     public int getRow() {
         return row;
     }
 
+    /**
+     * {@return the column coordinate of the position}
+     */
     public int getCol() {
         return col;
     }
 
+    /**
+     * {@return @return the position whose vertical and horizontal distances from this
+     * position are equal to the coordinate changes of the direction given}
+     *
+     * @param direction a direction that specifies a change in the coordinates
+     */
     public Position getTarget(Direction direction) {
         return new Position(row + direction.getColChange(), col + direction.getColChange());
     }
@@ -55,6 +76,11 @@ public class Position implements Cloneable {
         return getTarget(Direction.NORTHWEST);
     }
 
+    /**
+     * Changes the position by the coordinate changes of the direction given.
+     *
+     * @param direction a direction that specifies a change in the coordinates
+     */
     public void setTarget(Direction direction) {
         row += direction.getRowChange();
         col += direction.getColChange();
