f51d13c33674c757a46c50b5a864c94d873b8213
Bartha Zoltán <barthazoli00@gmail.com>
2021-05-10 18:03:26 +0200

    Added JUnit tests

---
+++ src/test/java/lepegeto/state/DirectionTest.java
@@ -0,0 +1,24 @@
+package lepegeto.state;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DirectionTest {
+
+    @Test
+    void of() {
+        assertSame(Direction.NORTH, Direction.of(-1, 0));
+        assertSame(Direction.EAST, Direction.of(0, 1));
+        assertSame(Direction.SOUTH, Direction.of(1, 0));
+        assertSame(Direction.WEST, Direction.of(0, -1));
+        assertSame(Direction.NORTHEAST, Direction.of(-1, 1));
+        assertSame(Direction.SOUTHEAST, Direction.of(1, 1));
+        assertSame(Direction.SOUTHWEST, Direction.of(1, -1));
+        assertSame(Direction.NORTHWEST, Direction.of(-1, -1));
+    }
+
+    @Test
+    void of_shouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> Direction.of(0,0));
+    }
+}
---
+++ src/test/java/lepegeto/state/PlayerTest.java
@@ -0,0 +1,14 @@
+package lepegeto.state;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void other() {
+        assertEquals(Player.RED, Player.RED.other().other());
+        assertNotEquals(Player.RED, Player.RED.other());
+    }
+}
