ed69152b26e5ed60a0b610de4b506c639dcd8090
Bartha Zoltán <barthazoli00@gmail.com>
2021-05-15 18:30:37 +0200

    Added Main.java

---
+++ src/main/java/lepegeto/javafx/Main.java
@@ -0,0 +1,9 @@
+package lepegeto.javafx;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(LepegetoApplication.class, args);
+    }
+}
