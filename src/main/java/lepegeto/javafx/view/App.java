4a6d0ecaec2d8e8d6120c906f012b1cf329004ae
Bartha Zoltán <barthazoli00@gmail.com>
2021-05-17 20:30:36 +0200

    Javadoc for App.java

--- src/main/java/lepegeto/javafx/view/App.java
+++ src/main/java/lepegeto/javafx/view/App.java
@@ -2,7 +2,14 @@ package lepegeto.javafx.view;
 
 import javafx.application.Application;
 
+/**
+ * The Main class of the program.
+ */
 public class App {
+    /**
+     *  Launches the JavaFX {@link LepegetoApplication}.
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         Application.launch(LepegetoApplication.class, args);
     }
