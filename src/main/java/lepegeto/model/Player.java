4ea319138a834683de0028f19cb9cb8d6465daac
Bartha Zoltán <barthazoli00@gmail.com>
2021-05-17 20:29:27 +0200

    Javadoc for Player.java

--- src/main/java/lepegeto/model/Player.java
+++ src/main/java/lepegeto/model/Player.java
@@ -8,9 +8,19 @@ import jakarta.xml.bind.annotation.*;
 @XmlType
 @XmlEnum
 public enum Player {
+    /**
+     * Blue player.
+     */
     @XmlEnumValue("Blue") BLUE,
+    /**
+     * Red player.
+     */
     @XmlEnumValue("Red") RED;
 
+    /**
+     * Returns the player that is not this one.
+     * @return the other player
+     */
     public Player other() {
         if(this.equals(RED)) {
             return BLUE;
