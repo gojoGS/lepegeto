cb05db6be4d07150419c91c9ce5a257a3dba5462
Bartha Zoltán <barthazoli00@gmail.com>
2021-05-17 20:29:35 +0200

    Javadoc for Owner.java

--- src/main/java/lepegeto/model/Owner.java
+++ src/main/java/lepegeto/model/Owner.java
@@ -1,8 +1,23 @@
 package lepegeto.model;
 
+/**
+ * Enum representing the owner of a cell on a board.
+ */
 public enum Owner {
+    /**
+     * Blue cell.
+     */
     BLUE,
+    /**
+     * Red cell.
+     */
     RED,
+    /**
+     * Forbidden cell.
+     */
     FORBIDDEN,
+    /**
+     * Free cell.
+     */
     NONE;
 }
