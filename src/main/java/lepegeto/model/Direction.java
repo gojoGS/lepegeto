811cd9b51a68017d84b893b0f26dfa5654ee65bf
Bartha Zoltán <barthazoli00@gmail.com>
2021-05-17 20:30:23 +0200

    Javadoc for Direction.java

--- src/main/java/lepegeto/model/Direction.java
+++ src/main/java/lepegeto/model/Direction.java
@@ -4,38 +4,77 @@ package lepegeto.model;
  * Represents the four main and the four secondary directions.
  */
 public enum Direction {
+    /**
+     * Direction of up.
+     */
     NORTH(-1, 0),
+    /**
+     * Direction of up and right.
+     */
     NORTHEAST(-1, 1),
+    /**
+     * Direction of right.
+     */
     EAST(0,1),
+    /**
+     * Direction of down and right.
+     */
     SOUTHEAST(1, 1),
+    /**
+     * Direction of down.
+     */
     SOUTH(1, 0),
+    /**
+     * Direction of down and left.
+     */
     SOUTHWEST(1, -1),
+    /**
+     * Direction of left.
+     */
     WEST(0, -1),
+    /**
+     * Direction of up and left.
+     */
     NORTHWEST(-1, -1);
 
+    /**
+     * Difference in Y coordinates.
+     */
     private final int rowChange;
+    /**
+     * Difference in X coordinates.
+     */
     private final int colChange;
+
+    /**
+     * Initializes a {@link Direction} with these two differences.
+     * @param rowChange change in Y coordinates
+     * @param colChange change in X coordinates
+     */
     Direction(int rowChange, int colChange) {
         this.rowChange = rowChange;
         this.colChange = colChange;
     }
 
     /**
-     * {@return the change in the row coordinate when moving to the direction}
+     * Returns the column change of the {@link Direction}.
+     * @return the change in the row coordinate when moving to the direction
      */
     public int getColChange() {
         return colChange;
     }
 
     /**
-     * {@return the change in the column coordinate when moving to the direction}
+     * Returns the row change of the {@link Direction}.
+     * @return the change in the column coordinate when moving to the direction
      */
     public int getRowChange() {
         return rowChange;
     }
 
     /**
-     * {@return the direction that corresponds to the coordinate changes specified}
+     * Creates a {@link Direction} from two ints.
+     * @return the direction that corresponds to the coordinate changes specified.
      *
      * @param rowChange the change in the row coordinate
      * @param colChange the change in the column coordinate
